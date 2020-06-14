5d5c94cd1ac5d48cfdc1f9d087c713acd5d35cd1
Ivaylo Tsochev <ivot@Ivaylos-MacBook-Pro.local>
2020-06-15 00:37:59 +0300

    Updates

---
+++ precache-manifest.037d31ce42a5244c640a5d51a410401e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af053ceb32be9a2ed69f49882f331283",
+    "url": "/React-to-do-list/index.html"
+  },
+  {
+    "revision": "a257dcdc6d25caef4d15",
+    "url": "/React-to-do-list/static/css/main.8b1ff8c5.chunk.css"
+  },
+  {
+    "revision": "bd9a80fd9d39edf2e529",
+    "url": "/React-to-do-list/static/js/2.1438f09a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-to-do-list/static/js/2.1438f09a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a257dcdc6d25caef4d15",
+    "url": "/React-to-do-list/static/js/main.dc0658e9.chunk.js"
+  },
+  {
+    "revision": "73542e63cd4dc3cf0e89",
+    "url": "/React-to-do-list/static/js/runtime-main.7d8786a4.js"
+  }
+]);
